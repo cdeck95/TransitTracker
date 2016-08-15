2a916244e66e694a5abc297ca7abdc5480e27753
cdeck95 <cdeck20@optonline.net>
2016-08-15 13:38:22 -0400

    Updated JS
    Fixed spacing

--- Contact.js
+++ Contact.js
@@ -1,34 +1,24 @@
 $( document ).on( "ready", function(){
-
 	runStuff();
-
 });
 
+function runStuff() {		
+	$('#Home').click(function() {
+		window.location = "index.html";
+	});
 
+	$('#Contact').click(function() {
+		window.location = "Contact.html";
+	});
 
-function runStuff() {	
-	
-		
-$('#Home').click(function() {
+	$('#About').click(function() {
+		window.location = "About.html";
+	});
 
-	window.location = "index.html";
-});
-
-$('#Contact').click(function() {
-
-	window.location = "Contact.html";
-});
-
-$('#About').click(function() {
-
-	window.location = "About.html";
-});
-
-$('#Error').click(function() {
-
-	window.location = "Error.html";
-});
-
-
+	$('#Error').click(function() {
+		window.location = "Error.html";
+	});
 }
+
+
 
